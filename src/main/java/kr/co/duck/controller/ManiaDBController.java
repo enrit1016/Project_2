ae6a6dce5438801547343605e9c9d016ca920eff
enrit1016 <71421722+enrit1016@users.noreply.github.com>
2024-09-25 15:24:03 +0900

    ManiaDB 검색 코드 완성 검색된 결과 상세페이지로 넘기는 작업이 필요. link 또는 guid의 값에 스크래핑해야하는 사이트의 링크를 저장해두었음.

--- src/main/java/kr/co/duck/controller/ManiaDBController.java
+++ src/main/java/kr/co/duck/controller/ManiaDBController.java
@@ -25,15 +25,17 @@ public class ManiaDBController {
 
     // 검색 요청을 처리하는 매핑
     @GetMapping("/temp/search")
-    public String searchManiaDB(@RequestParam("query") String query, Model model) {
-    	
-    	// ManiaDBService를 사용하여 검색 수행
-    	
-        List<Music> musicList = maniaDBService.searchMusic(query);
-        
+    public String searchManiaDB(@RequestParam("query") String query,
+                                @RequestParam(value = "searchType", defaultValue = "song") String searchType, 
+                                Model model) {
+
+        // searchType에 따른 데이터 검색 수행
+        List<?> searchResult = maniaDBService.searchMusic(query, searchType);
+
         // 검색 결과를 모델에 추가
-        model.addAttribute("musicList", musicList);
-        
+        model.addAttribute("searchResult", searchResult);
+        model.addAttribute("searchType", searchType);  // 검색 유형도 추가 (song, artist, album)
+
         // 검색 결과를 포함한 페이지를 다시 렌더링
         return "temp/maniadbSearch"; // JSP 파일의 위치
     }
--- src/main/java/kr/co/duck/service/ManiaDBService.java
+++ src/main/java/kr/co/duck/service/ManiaDBService.java
@@ -1,6 +1,7 @@
 package kr.co.duck.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,25 +13,32 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ManiaDBService {
 
-	private static final String API_URL = "https://www.maniadb.com/api/search/%s/?sr=song&key=example&v=0.5";
+	private static final String API_URL = "https://www.maniadb.com/api/search/%s/?sr=%s&key=example&v=0.5";
 
-	public List<Music> searchMusic(String query) {
+	public List<?> searchMusic(String query, String searchType) {
 		// API 호출 및 결과 가져오기 (XML 형식으로)
-		String xmlResult = callManiaDBApi(query);
-
+		String xmlResult = callManiaDBApi(query, searchType);
 		// XML 결과가 비어 있거나 처리할 수 없는 경우 처리
 		if (xmlResult.isEmpty()) {
 			System.out.println("API 호출 실패 또는 응답이 비어 있습니다: " + xmlResult);
 			return new ArrayList<>(); // 빈 리스트 반환
 		}
 
-		// XML을 List<Music>으로 변환
-		return parseXmlToMusicList(xmlResult);
+		// searchType에 따른 처리
+		switch (searchType.toLowerCase()) {
+		case "artist":
+			return parseXmlToArtistList(xmlResult);
+		case "album":
+			return parseXmlToAlbumList(xmlResult);
+		case "song":
+		default:
+			return parseXmlToMusicList(xmlResult);
+		}
 	}
 
-	private String callManiaDBApi(String query) {
+	private String callManiaDBApi(String query, String searchType) {
 		RestTemplate restTemplate = new RestTemplate();
-		String url = String.format(API_URL, query);
+		String url = String.format(API_URL, query, searchType);
 		try {
 			return restTemplate.getForObject(url, String.class);
 		} catch (Exception e) {
@@ -46,35 +54,118 @@ public class ManiaDBService {
 			xmlResult = xmlResult.replaceAll(":", "");
 			xmlResult = xmlResult.replaceAll("//", "://");
 			Document doc = Jsoup.parse(xmlResult, "", org.jsoup.parser.Parser.xmlParser());
-			//System.out.println(doc);
+
 			// XML에서 item 요소를 선택
 			Elements items = doc.select("item");
 			for (Element item : items) {
-				// 각 item 요소에서 필요한 데이터를 추출
-				String albumTitle = item.select("maniadbalbum title").text();				
+				String albumTitle = item.select("maniadbalbum title").text();
 				String title = item.select("title").text();
 				title = title.replace(albumTitle, "");
-				String link = item.select("link").text();
+				String guid = item.select("guid").text();
 				String albumImage = item.select("image").text();
 				String albumArtist = item.select("maniadbtrackartists maniadbartist name").text();
-				
-			
+
 				// Music 객체 생성 및 데이터 설정
 				Music music = new Music();
 				music.setTitle(title);
-				music.setLink(link);
+				music.setGuid(guid);
 				music.setAlbumTitle(albumTitle);
 				music.setAlbumImage(albumImage);
 				music.setAlbumArtist(albumArtist);
 
-				
 				// 리스트에 추가
 				musicList.add(music);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 		return musicList;
+	}
+
+	private List<Artist> parseXmlToArtistList(String xmlResult) {
+		List<Artist> artistList = new ArrayList<>();
+		try {
+			// Jsoup을 사용하여 XML 파싱
+			xmlResult = xmlResult.replaceAll(":", "");
+			xmlResult = xmlResult.replaceAll("//", "://");
+			Document doc = Jsoup.parse(xmlResult, "", org.jsoup.parser.Parser.xmlParser());
+			// XML에서 item 요소를 선택
+			Elements items = doc.select("item");
+			for (Element item : items) {
+				String artistName = item.select("title").text();
+				String period = item.select("period").text();
+				String image = item.select("image").text();
+				String majorsonglistRaw = item.select("maniadbmajorsonglist").text();
+				String relatedartistlist = item.select("maniadbrelatedartistlist").text();
+				String description = item.select("description").text();
+				String link = item.select("link").text();
+				System.out.println(link);
+				// 트랙 리스트를 분할하여 저장
+				String[] majorsonglistArray = majorsonglistRaw.split(" / ");
+				List<String> majorSongList = Arrays.asList(majorsonglistArray);
+
+				// Artist 객체 생성 및 데이터 설정
+				Artist artist = new Artist();
+				artist.setArtistName(artistName);
+				artist.setPeriod(period);
+				artist.setImage(image);
+				artist.setMajorSongList(majorSongList);
+				artist.setRelativDartistList(relatedartistlist);
+				artist.setDescription(description);
+				artist.setLink(link);
+
+				// 리스트에 추가
+				artistList.add(artist);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return artistList;
+	}
+
+	private List<Album> parseXmlToAlbumList(String xmlResult) {
+		List<Album> albumList = new ArrayList<>();
+		try {
+			// Jsoup을 사용하여 XML 파싱
+			xmlResult = xmlResult.replaceAll(":", "");
+			xmlResult = xmlResult.replaceAll("//", "://");
+			xmlResult = xmlResult.replace("[Disc 1]", "");
+			xmlResult = xmlResult.replace("[Disc 2]", " / [Disc 2]");
+			Document doc = Jsoup.parse(xmlResult, "", org.jsoup.parser.Parser.xmlParser());
+
+			// XML에서 item 요소를 선택
+			Elements items = doc.select("item");
+			for (Element item : items) {
+				String albumArtist = item.select("maniadbartist name").text();
+				String albumName = item.select("title").text();
+				String releaseCompany = item.select("release_company").text();
+				String albumimage = item.select("front image").text();
+				String trackListRaw = item.select("maniadbtracklist").text();
+				String description = item.select("description").text();
+				String guid = item.select("guid").text();
+
+				// 트랙 리스트를 '/' 단위로 분할하여 배열로 저장
+				String[] trackListArray = trackListRaw.split(" / ");
+				List<String> trackList = Arrays.asList(trackListArray);
+
+				// Album 객체 생성 및 데이터 설정
+				Album album = new Album();
+				album.setAlbumArtist(albumArtist);
+				album.setAlbumName(albumName);
+				album.setReleaseCompany(releaseCompany);
+				album.setAlbumimage(albumimage);
+				album.setTrackList(trackList);
+				album.setDescription(description);
+				album.setGuid(guid);
+				
+				// 리스트에 추가
+				albumList.add(album);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return albumList;
 	}
 
 	// Music 클래스 정의
@@ -87,7 +178,16 @@ public class ManiaDBService {
 		private String description;
 		private String albumArtist;
 		private String albumRelease;
-		
+		private String guid; //정보를 가진 페이지로 넘어가는 링크
+
+		public String getGuid() {
+			return guid;
+		}
+
+		public void setGuid(String guid) {
+			this.guid = guid;
+		}
+
 		// Getters and Setters
 		public String getTitle() {
 			return title;
@@ -152,6 +252,146 @@ public class ManiaDBService {
 		public void setAlbumRelease(String albumRelease) {
 			this.albumRelease = albumRelease;
 		}
+	}
+
+	// Artist 클래스 정의
+	public static class Artist {
+		private String artistName;
+		private String period;
+		private String image;
+		private List<String> majorSongList;
+		private String relativDartistList;
+		private String description;
+		private String link; //정보를 가진 페이지로 넘어가는 링크
+		
+		
+		
+		public String getLink() {
+			return link;
+		}
+
+		public void setLink(String link) {
+			this.link = link;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getArtistName() {
+			return artistName;
+		}
+
+		public void setArtistName(String artistName) {
+			this.artistName = artistName;
+		}
+
+		public String getPeriod() {
+			return period;
+		}
+
+		public void setPeriod(String period) {
+			this.period = period;
+		}
+
+		public String getImage() {
+			return image;
+		}
+
+		public void setImage(String image) {
+			this.image = image;
+		}
+
+		public List<String> getMajorSongList() {
+			return majorSongList;
+		}
+
+		public void setMajorSongList(List<String> majorSongList) {
+			this.majorSongList = majorSongList;
+		}
+
+		public String getRelativDartistList() {
+			return relativDartistList;
+		}
+
+		public void setRelativDartistList(String relativDartistList) {
+			this.relativDartistList = relativDartistList;
+		}
 
 	}
+
+	// Album 클래스 정의
+	public static class Album {
+		private String albumName;
+		private String albumArtist;
+		private String releaseCompany;
+		private List<String> trackList;
+		private String albumimage;
+		private String description;
+		private String guid; //정보를 가진 페이지로 넘어가는 링크
+
+		
+		
+		public String getGuid() {
+			return guid;
+		}
+
+		public void setGuid(String guid) {
+			this.guid = guid;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getAlbumName() {
+			return albumName;
+		}
+
+		public void setAlbumName(String albumName) {
+			this.albumName = albumName;
+		}
+
+		public String getAlbumArtist() {
+			return albumArtist;
+		}
+
+		public void setAlbumArtist(String albumArtist) {
+			this.albumArtist = albumArtist;
+		}
+
+		public String getReleaseCompany() {
+			return releaseCompany;
+		}
+
+		public void setReleaseCompany(String releaseCompany) {
+			this.releaseCompany = releaseCompany;
+		}
+
+		public List<String> getTrackList() {
+			return trackList;
+		}
+
+		public void setTrackList(List<String> trackList) {
+			this.trackList = trackList;
+		}
+
+		public String getAlbumimage() {
+			return albumimage;
+		}
+
+		public void setAlbumimage(String albumimage) {
+			this.albumimage = albumimage;
+		}
+
+	}
+
 }
